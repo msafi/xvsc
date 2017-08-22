30926723bc7c2f9a05fb6cd368c1b9f8345417e1
M.K. Safi <msafi@msafi.com>
2017-08-21 21:01:55 -0700

    Clean up theme

---
+++ trailscasts/sampleFiles/ajaxRequest.js
@@ -0,0 +1,20 @@
+var globalVar;
+/**
+ * Constructor for <code>AjaxRequest</code> class
+ * @param url the url for the request<p/>
+ */
+function AjaxRequest(url) {
+  var urls = ["www.cnn.com", 5, globalVar];
+  this.request = new XMLHttpRequest();
+  url = url.replace(/^\s*(.*)/, "$1"); // skip leading whitespace
+  /* check the url to be in urls */
+  var a = "\u1111\z\n\u11";
+  this.foo = new function () {
+  };
+  foo();
+
+  var hello = () => console.log("hello")
+}
+
+class NameClass {
+}
