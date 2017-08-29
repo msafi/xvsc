0f7d2f441075ab0a467413b4f1c7816cb5642188
M.K. Safi <msafi@msafi.com>
2017-08-29 13:51:03 -0700

    Add "keep editor" keybinding and fix editor group layout change bug

--- xvscode-misc/src/extension.ts
+++ xvscode-misc/src/extension.ts
@@ -1,5 +1,5 @@
 'use strict';
-import {ExtensionContext, commands} from 'vscode';
+import {ExtensionContext, commands, window} from 'vscode';
 import {openInSourceTree} from './openInSourceTree'
 import {deleteToTextBeginning} from './deleteToTextBeginning'
 
@@ -7,11 +7,22 @@ export function activate(context: ExtensionContext) {
   const withContext = (fn: Function) => (...args) => fn(context, ...args)
   const {registerCommand, registerTextEditorCommand} = commands
   const storage = context.workspaceState
+  const isActiveEditor = window.activeTextEditor !== undefined
   const hasSwitchedLayout = storage.get('hasSwitched')
-
-  if (!hasSwitchedLayout) {
-    commands.executeCommand('workbench.action.toggleEditorGroupLayout')
-    storage.update('hasSwitched', true)
+  const handleLayoutSwitch = () => {
+    if (!hasSwitchedLayout) {
+      commands.executeCommand('workbench.action.toggleEditorGroupLayout')
+      storage.update('hasSwitched', true)
+    }
+  }
+  
+  if (isActiveEditor) {
+    handleLayoutSwitch()
+  } else {
+    const registration = window.onDidChangeActiveTextEditor(() => {
+      handleLayoutSwitch()
+      registration.dispose()
+    })
   }
   
   context.subscriptions.push(
