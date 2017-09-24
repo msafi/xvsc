fe99fadeabec5267ea729f902e926d25c56fee10
M.K. Safi <msafi@msafi.com>
2017-09-24 16:15:01 -0700

    Prettierify `openInSourceTree.ts`

--- xvscMisc/src/openInSourceTree.ts
+++ xvscMisc/src/openInSourceTree.ts
@@ -1,29 +1,26 @@
-import * as vscode from 'vscode'
-import * as shelljs from 'shelljs'
-import * as _ from 'lodash'
+import * as vscode from 'vscode';
+import * as shelljs from 'shelljs';
+import * as _ from 'lodash';
 
-const sourceTreeCmd = 'open -a /Applications/SourceTree.app/Contents/MacOS/SourceTree'
+const sourceTreeCmd = 'open -a /Applications/SourceTree.app/Contents/MacOS/SourceTree';
 
 export const openInSourceTree = () => {
   try {
-    const rootFolder = _.get(vscode, 'workspace.workspaceFolders.0.uri.fsPath') as string
+    const rootFolder = _.get(vscode, 'workspace.workspaceFolders.0.uri.fsPath') as string;
 
     if (!rootFolder) {
-      throw new Error('Could not find root folder')
+      throw new Error('Could not find root folder');
     }
 
-    shelljs.cd(rootFolder)
+    shelljs.cd(rootFolder);
 
-    const gitRepoPath = shelljs.exec('git rev-parse --show-toplevel')
+    const gitRepoPath = shelljs.exec('git rev-parse --show-toplevel');
 
-    vscode.window.withProgress(
-      {location: vscode.ProgressLocation.Window},
-      (progress) => {
-        progress.report({message: 'Opening git repo in Source Tree'})
-        return new Promise((resolve) => shelljs.exec(`${sourceTreeCmd} ${gitRepoPath}`, resolve))
-      },
-    )
-  } catch(e) {
-    vscode.window.showInformationMessage('Could not open git repo in Source Tree')
+    vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, progress => {
+      progress.report({ message: 'Opening git repo in Source Tree' });
+      return new Promise(resolve => shelljs.exec(`${sourceTreeCmd} ${gitRepoPath}`, resolve));
+    });
+  } catch (e) {
+    vscode.window.showInformationMessage('Could not open git repo in Source Tree');
   }
-}
+};
