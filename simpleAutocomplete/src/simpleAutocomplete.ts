81267dabb9e145559bb282eb5c81604709e9384b
M.K. Safi <msafi@msafi.com>
2017-09-23 22:05:37 -0700

    Style Simple Autocomplete with Prettier

--- simpleAutocomplete/src/simpleAutocomplete.ts
+++ simpleAutocomplete/src/simpleAutocomplete.ts
@@ -1,60 +1,57 @@
-import {TextEditor, workspace} from 'vscode'
-import {documentRippleScanner} from './documentRippleScanner'
-import {tokenizer} from './tokenizer'
-import {fuzzySearch} from './fuzzySearch'
+import { TextEditor, workspace } from 'vscode';
+import { documentRippleScanner } from './documentRippleScanner';
+import { tokenizer } from './tokenizer';
+import { fuzzySearch } from './fuzzySearch';
 
 export class SimpleAutocomplete {
-  state: {
-    needle: string,
-    nextIterator: IterableIterator<boolean> | undefined,
-    preventReset: boolean,
-    discardedMatches: string[],
-    isActive: boolean,
-  }
+  private state: {
+    needle: string;
+    nextIterator: IterableIterator<boolean> | undefined;
+    preventReset: boolean;
+    discardedMatches: string[];
+    isActive: boolean;
+  };
 
   constructor() {
-    this.next = this.next.bind(this)
-    this.reset = this.reset.bind(this)
+    this.next = this.next.bind(this);
+    this.reset = this.reset.bind(this);
 
-    this.reset()
+    this.reset();
   }
 
   public reset() {
-    if (
-      (!this.state) ||
-      (this.state.preventReset !== true && this.state.isActive)
-    ) {
+    if (!this.state || (this.state.preventReset !== true && this.state.isActive)) {
       this.state = {
         needle: '',
         nextIterator: undefined,
         preventReset: false,
         discardedMatches: [],
         isActive: false,
-      }
+      };
     }
   }
 
   public next(activeTextEditor: TextEditor) {
-    this.state.isActive = true
+    this.state.isActive = true;
 
     if (this.canAutocomplete(activeTextEditor)) {
       if (!this.state.nextIterator) {
-        this.state.nextIterator = this.nextGenerator(activeTextEditor)
+        this.state.nextIterator = this.nextGenerator(activeTextEditor);
       }
 
-      const nextResult = this.state.nextIterator.next()
+      const nextResult = this.state.nextIterator.next();
 
       if (nextResult.done) {
-        this.setMatch(this.state.needle, activeTextEditor).then(this.reset)
+        this.setMatch(this.state.needle, activeTextEditor).then(this.reset);
       }
     } else {
-      this.reset()
+      this.reset();
     }
   }
 
   private canAutocomplete(activeTextEditor: TextEditor) {
-    const {selection, document} = activeTextEditor
-    const wordRange = document.getWordRangeAtPosition(selection.end)
+    const { selection, document } = activeTextEditor;
+    const wordRange = document.getWordRangeAtPosition(selection.end);
 
     if (
       wordRange === undefined ||
@@ -62,62 +59,62 @@ export class SimpleAutocomplete {
       selection.start.line !== selection.end.line ||
       selection.start.character !== selection.end.character
     ) {
-      return false
+      return false;
     } else {
-      return true
+      return true;
     }
   }
 
   private *nextGenerator(activeTextEditor: TextEditor) {
-    this.setNeedle(activeTextEditor)
+    this.setNeedle(activeTextEditor);
 
     if (!this.state.needle) {
-      return
+      return;
     }
 
-    const {document, selection} = activeTextEditor
-    const documentIterator = documentRippleScanner(document, selection.end.line)
+    const { document, selection } = activeTextEditor;
+    const documentIterator = documentRippleScanner(document, selection.end.line);
     for (const line of documentIterator) {
-      const wordSeparators = workspace.getConfiguration().editor.wordSeparators
-      const tokensIterator = tokenizer(line.text, wordSeparators)
+      const wordSeparators = workspace.getConfiguration().editor.wordSeparators;
+      const tokensIterator = tokenizer(line.text, wordSeparators);
 
       for (const token of tokensIterator) {
         if (
           fuzzySearch(this.state.needle.toLowerCase(), token.toLowerCase()) &&
           this.state.discardedMatches.indexOf(token) === -1
         ) {
-          this.state.discardedMatches.push(token)
-          this.setMatch(token, activeTextEditor)
-          yield true
+          this.state.discardedMatches.push(token);
+          this.setMatch(token, activeTextEditor);
+          yield true;
         }
       }
     }
   }
 
   private setNeedle(activeTextEditor: TextEditor) {
-    const {document, selection} = activeTextEditor
-    const needle = document.getText(document.getWordRangeAtPosition(selection.end))
+    const { document, selection } = activeTextEditor;
+    const needle = document.getText(document.getWordRangeAtPosition(selection.end));
 
     if (typeof needle === 'string') {
-      this.state.discardedMatches.push(needle)
-      this.state.needle = needle
+      this.state.discardedMatches.push(needle);
+      this.state.needle = needle;
     }
   }
 
   private async setMatch(match: string, activeTextEditor: TextEditor) {
-    const {selection, document} = activeTextEditor
+    const { selection, document } = activeTextEditor;
 
-    const wordRange = document.getWordRangeAtPosition(selection.end)
+    const wordRange = document.getWordRangeAtPosition(selection.end);
 
     if (wordRange) {
-      this.state.preventReset = true
+      this.state.preventReset = true;
 
-      await activeTextEditor.edit((editBuilder) => {
-        editBuilder.delete(wordRange)
-        editBuilder.insert(selection.end, match)
-      })
+      await activeTextEditor.edit(editBuilder => {
+        editBuilder.delete(wordRange);
+        editBuilder.insert(selection.end, match);
+      });
 
-      this.state.preventReset = false
+      this.state.preventReset = false;
     }
   }
 }
--- simpleAutocomplete/test/integration/extension.test.ts
+++ simpleAutocomplete/test/integration/extension.test.ts
@@ -1,114 +1,104 @@
 /* tslint:disable */
-import {
-  window,
-  Uri,
-  Position,
-  Selection,
-  TextEditor,
-  commands,
-} from 'vscode'
-import * as assert from 'assert'
-import * as path from 'path'
+import { window, Uri, Position, Selection, TextEditor, commands } from 'vscode';
+import * as assert from 'assert';
+import * as path from 'path';
 
 describe('Simple Autocomplete', () => {
-  let textEditor: TextEditor
+  let textEditor: TextEditor;
 
   before(() => {
-    return window.showTextDocument(
-      Uri.file(path.join(__dirname, '..', '..', '..', 'test', 'fixtures', 'one.js'))
-    ).then((_textEditor) => {
-      textEditor = _textEditor
-      return
-    })
-  })
+    return window
+      .showTextDocument(
+        Uri.file(path.join(__dirname, '..', '..', '..', 'test', 'fixtures', 'one.js')),
+      )
+      .then(_textEditor => {
+        textEditor = _textEditor;
+        return;
+      });
+  });
 
   beforeEach(() => {
-    textEditor.selection = new Selection(new Position(7, 35), new Position(7, 35))
-    return commands.executeCommand('workbench.action.files.revert')
-  })
+    textEditor.selection = new Selection(new Position(7, 35), new Position(7, 35));
+    return commands.executeCommand('workbench.action.files.revert');
+  });
 
   it('it can cycle to the next word', async () => {
-    const {document, selection} = textEditor
+    const { document, selection } = textEditor;
 
-    const currentWord = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(currentWord === 'someva')
+    const currentWord = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(currentWord === 'someva');
 
-    await commands.executeCommand('simpleAutocomplete.next')
+    await commands.executeCommand('simpleAutocomplete.next');
 
-    const nextWord = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(nextWord === 'somevak')
-  })
+    const nextWord = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(nextWord === 'somevak');
+  });
 
   it('does not cycle when the cursor is not at a word boundary', async () => {
-    const { document, selection } = textEditor
-    textEditor.selection = new Selection(new Position(7, 34), new Position(7, 34))
+    const { document, selection } = textEditor;
+    textEditor.selection = new Selection(new Position(7, 34), new Position(7, 34));
 
-    const currentWord = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(currentWord === 'someva')
+    const currentWord = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(currentWord === 'someva');
 
-    await commands.executeCommand('simpleAutocomplete.next')
+    await commands.executeCommand('simpleAutocomplete.next');
 
-    const nextWord = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(nextWord === 'someva')
-  })
+    const nextWord = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(nextWord === 'someva');
+  });
 
   it('does not suggest the same result more than once', async () => {
     // await _delay(4000)
-    const { document, selection } = textEditor
+    const { document, selection } = textEditor;
 
-    const _1 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_1 === 'someva')
+    const _1 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_1 === 'someva');
 
-    await commands.executeCommand('simpleAutocomplete.next')
-    const _2 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_2 === 'somevak')
+    await commands.executeCommand('simpleAutocomplete.next');
+    const _2 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_2 === 'somevak');
 
-    await commands.executeCommand('simpleAutocomplete.next')
-    const _3 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_3 === 'someVariable')
+    await commands.executeCommand('simpleAutocomplete.next');
+    const _3 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_3 === 'someVariable');
 
-    await commands.executeCommand('simpleAutocomplete.next')
-    const _4 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_4 === 'someVariableFoo')
-  })
+    await commands.executeCommand('simpleAutocomplete.next');
+    const _4 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_4 === 'someVariableFoo');
+  });
 
   it('cycles back to the needle when results are exhausted', async () => {
     // await _delay(4000)
-    const { document, selection } = textEditor
+    const { document, selection } = textEditor;
 
-    const _1 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_1 === 'someva')
+    const _1 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_1 === 'someva');
 
-    await commands.executeCommand('simpleAutocomplete.next')
-    await commands.executeCommand('simpleAutocomplete.next')
-    await commands.executeCommand('simpleAutocomplete.next')
-    await commands.executeCommand('simpleAutocomplete.next')
+    await commands.executeCommand('simpleAutocomplete.next');
+    await commands.executeCommand('simpleAutocomplete.next');
+    await commands.executeCommand('simpleAutocomplete.next');
+    await commands.executeCommand('simpleAutocomplete.next');
 
-    const _2 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_2 === 'someva')
-  })
+    const _2 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_2 === 'someva');
+  });
 
-  it.only('does not do anything if the user invokes the command while making a selection', async () => {
+  it('does not do anything if the user invokes the command while making a selection', async () => {
     // await _delay(4000)
-    const { document, selection } = textEditor
-    textEditor.selection = new Selection(new Position(7, 29), new Position(7, 35))
+    const { document, selection } = textEditor;
+    textEditor.selection = new Selection(new Position(7, 29), new Position(7, 35));
 
-    const _1 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_1 === 'someva')
+    const _1 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_1 === 'someva');
 
-    await commands.executeCommand('simpleAutocomplete.next')
-    const _2 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_2 === 'someva')
+    await commands.executeCommand('simpleAutocomplete.next');
+    const _2 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_2 === 'someva');
 
-    textEditor.selection = new Selection(new Position(7, 35), new Position(7, 35))
+    textEditor.selection = new Selection(new Position(7, 35), new Position(7, 35));
 
-    await commands.executeCommand('simpleAutocomplete.next')
-    const _3 = document.getText(document.getWordRangeAtPosition(selection.end))
-    assert(_3 === 'somevak')
-  })
-})
-
-/*tslint:disable-next-line*/
-// function _delay(t: number) {
-//   return new Promise(resolve => setTimeout(resolve, t))
-// }
+    await commands.executeCommand('simpleAutocomplete.next');
+    const _3 = document.getText(document.getWordRangeAtPosition(selection.end));
+    assert(_3 === 'somevak');
+  });
+});
